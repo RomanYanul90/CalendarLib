ffb94575d863e06066bf39df669222c0e4e32ab0
Roman <roman@inspr.com>
2020-12-08 23:06:27 +0300

    New method for set event and time checking

--- src/scripts/dateValidator.js
+++ src/scripts/dateValidator.js
@@ -1,4 +1,4 @@
-export default function() {
+function dateValidator() {
 
     var currentDay = new Date().getDate()
     var currentMonth = new Date().getMonth() + 1
@@ -11,6 +11,63 @@ export default function() {
     currentHour > 10 ? currentHour : currentHour = '0' + currentHour
     currentMin > 10 ? currentMin : currentMin = '0' + currentMin
 
-    return [`${currentDay}.${currentMonth}.${currentYear}`, `${currentHour}:${currentMin}`]
+    return `${currentDay}.${currentMonth}.${currentYear} ${currentHour}:${currentMin}`
 }
 
+var calendar = (function () {
+
+    var events = []
+    var started = false
+
+    function setEvent(date, event, callback){
+
+        var newEvent={
+            event,
+            date,
+            callback,
+            eventIsDone: false
+        }
+
+        events.push(newEvent)
+
+        if (!started) {
+            console.log('Running')
+            started = true;
+            timeCheck();
+        }
+    }
+
+    function getEventsList(){
+        console.log(events)
+    }
+
+    function removeEvent(event) {
+        events = events.filter(el => el.event !== event)
+    }
+
+    function timeCheck(){
+        if (!events.length || events.every(event => event.eventIsDone)) {
+            started = false;
+            console.log('All events are completed')
+            return
+        }
+
+        var currentTime=dateValidator()
+
+        events.forEach((el)=>{
+            if(el.eventIsDone === false && el.date===currentTime){
+                el.callback()
+                el.eventIsDone = true
+            }
+        })
+
+        setTimeout(()=>{timeCheck()},1000)
+    }
+
+    return {
+        setEvent,
+        removeEvent,
+        getEventsList
+    }
+
+})()
