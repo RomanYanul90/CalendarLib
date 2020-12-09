08f076047a9de778715fc46d80dff6b07c0f31b7
Roman <roman@inspr.com>
2020-12-09 23:12:58 +0300

    new date format and events checking added

--- src/scripts/dateValidator.js
+++ src/scripts/dateValidator.js
@@ -1,32 +1,31 @@
-function dateValidator() {
-
-    var currentDay = new Date().getDate()
-    var currentMonth = new Date().getMonth() + 1
-    var currentYear = new Date().getFullYear()
-    var currentHour = new Date().getHours()
-    var currentMin = new Date().getMinutes()
-
-    currentDay > 10 ? currentDay : currentDay = '0' + currentDay
-    currentMonth > 10 ? currentMonth : currentMonth = '0' + currentMonth
-    currentHour > 10 ? currentHour : currentHour = '0' + currentHour
-    currentMin > 10 ? currentMin : currentMin = '0' + currentMin
-
-    return `${currentDay}.${currentMonth}.${currentYear} ${currentHour}:${currentMin}`
-}
-
 var calendar = (function () {
 
     var events = []
     var started = false
 
-    function setEvent(date, event, callback){
+    function setEvent(date, event, callback) {
 
-        var newEvent={
+        if (new Date(datePreparation(date)) < new Date()) {
+            console.log("The time you specified has already passed!")
+            return
+        }
+
+        var newEvent = {
             event,
-            date,
+            date: new Date(datePreparation(date)),
             callback,
             eventIsDone: false
         }
+
+        if (events.some(el => el.event === newEvent.event && el.date.toString() === newEvent.date.toString())) {
+            console.log("An event with the same name and time already exists")
+            return
+        }
+
+        // if(events.some(el=>el.event===event && el.date===new Date(datePreparation(date)).toString() )){
+        //     console.log("An event with the same name and time already exists")
+        //     return
+        // }
 
         events.push(newEvent)
 
@@ -37,38 +36,57 @@ var calendar = (function () {
         }
     }
 
-    function timeCheck(){
+    function timeCheck() {
         if (!events.length || events.every(el => el.eventIsDone)) {
             started = false;
             console.log('All events are completed')
             return
         }
 
-        var currentTime=dateValidator()
-
-        events.forEach((el)=>{
-            if(el.eventIsDone === false && el.date===currentTime){
+        events.forEach((el) => {
+            if (el.eventIsDone === false && el.date.toString() === new Date().toString()) {
                 el.callback()
                 el.eventIsDone = true
             }
         })
 
-        setTimeout(()=>{timeCheck()},1000)
+        setTimeout(() => {
+            timeCheck()
+        }, 1000)
     }
 
-    function getEventsList(){
-        console.log(events)
+    function datePreparation(userDate) {
+        var time = userDate.split(" ")[1]
+        var date = userDate.split(" ")[0].split('.')
+        date[1] = date.splice(0, 1, date[1])[0]
+        return `${date.join('.')} ${time}`
     }
+
+    function getEventsList(day) {
+        var result = events.map((el)=>{
+            if(el.date.toDateString()===new Date(datePreparation(day)).toDateString()){
+                return el
+            }
+        })
+        console.log(result)
+    }
+    // function getEventsList() {
+    //
+    //     console.log(events)
+    // }
+
 
     function removeEvent(eventToRemove) {
         events = events.filter(el => el.event !== eventToRemove)
     }
 
-    function changeEvent(event,newDate,newName){
-       return events.find((el)=>{if(el.event===event){
-            el.event=newName
-            el.date=newDate
-        }})
+    function changeEvent(event, date, newName = event, newDate = date) {
+        return events.find((el) => {
+            if (el.event === event && el.date.toString() === new Date(datePreparation(date)).toString()) {
+                el.event = newName
+                el.date = new Date(datePreparation(newDate))
+            }
+        })
     }
 
     return {
--- src/scripts/library.js
+++ src/scripts/library.js
@@ -1,94 +1,108 @@
-// var calendar = {
-//     events: [],
-//     dateValidator: function () {
-//         var currentDay = new Date().getDate()
-//         var currentMonth = new Date().getMonth() + 1
-//         var currentYear = new Date().getFullYear()
-//         var currentHour = new Date().getHours()
-//         var currentMin = new Date().getMinutes()
+// // var calendar = {
+// //     events: [],
+// //     dateValidator: function () {
+// //         var currentDay = new Date().getDate()
+// //         var currentMonth = new Date().getMonth() + 1
+// //         var currentYear = new Date().getFullYear()
+// //         var currentHour = new Date().getHours()
+// //         var currentMin = new Date().getMinutes()
+// //
+// //         currentDay > 10 ? currentDay : currentDay = '0' + currentDay
+// //         currentMonth > 10 ? currentMonth : currentMonth = '0' + currentMonth
+// //         currentHour > 10 ? currentHour : currentHour = '0' + currentHour
+// //         currentMin > 10 ? currentMin : currentMin = '0' + currentMin
+// //
+// //         return [`${currentDay}.${currentMonth}.${currentYear}`, `${currentHour}:${currentMin}`]
+// //     },
+// //     setEvent: function (date, time, event) {
+// //         var newEvent = {
+// //             event,
+// //             date,
+// //             time
+// //         }
+// //         if (!this.events.includes(newEvent)) {
+// //             this.events.push(newEvent)
+// //         }
+// //
+// //         [currentDate, currentTime] = this.dateValidator()
+// //
+// //         // console.log(currentDate, currentTime)
+// //
+// //         if (date === currentDate && time === currentTime) {
+// //             console.log(`It is ${event} time!`)
+// //             return
+// //         }
+// //         setTimeout(() => {
+// //             this.setEvent(date, time,event)
+// //         }, 1000)
+// //
+// //     },
+// //     removeEvent: function (eventToRemove) {
+// //         this.events=this.events.filter(el=>eventToRemove!==el.event)
+// //     },
+// //     getEvents: function () {
+// //         console.log(this.events)
+// //     }
+// // }
+// //
+// // // calendar.setEvent('06.12.2020','19:05','new year')
+// // calendar.getEvents()
 //
-//         currentDay > 10 ? currentDay : currentDay = '0' + currentDay
-//         currentMonth > 10 ? currentMonth : currentMonth = '0' + currentMonth
-//         currentHour > 10 ? currentHour : currentHour = '0' + currentHour
-//         currentMin > 10 ? currentMin : currentMin = '0' + currentMin
+// function dateValidator() {
 //
-//         return [`${currentDay}.${currentMonth}.${currentYear}`, `${currentHour}:${currentMin}`]
-//     },
-//     setEvent: function (date, time, event) {
-//         var newEvent = {
-//             event,
-//             date,
-//             time
-//         }
-//         if (!this.events.includes(newEvent)) {
-//             this.events.push(newEvent)
-//         }
+//     var currentDay = new Date().getDate()
+//     var currentMonth = new Date().getMonth() + 1
+//     var currentYear = new Date().getFullYear()
+//     var currentHour = new Date().getHours()
+//     var currentMin = new Date().getMinutes()
 //
-//         [currentDate, currentTime] = this.dateValidator()
+//     currentDay > 10 ? currentDay : currentDay = '0' + currentDay
+//     currentMonth > 10 ? currentMonth : currentMonth = '0' + currentMonth
+//     currentHour > 10 ? currentHour : currentHour = '0' + currentHour
+//     currentMin > 10 ? currentMin : currentMin = '0' + currentMin
 //
-//         // console.log(currentDate, currentTime)
+//     return `${currentDay}.${currentMonth}.${currentYear} ${currentHour}:${currentMin}`
 //
-//         if (date === currentDate && time === currentTime) {
-//             console.log(`It is ${event} time!`)
-//             return
-//         }
-//         setTimeout(() => {
-//             this.setEvent(date, time,event)
-//         }, 1000)
-//
-//     },
-//     removeEvent: function (eventToRemove) {
-//         this.events=this.events.filter(el=>eventToRemove!==el.event)
-//     },
-//     getEvents: function () {
-//         console.log(this.events)
-//     }
 // }
 //
-// // calendar.setEvent('06.12.2020','19:05','new year')
-// calendar.getEvents()
+// var events=[]
+//
+// function setEvent(date, event, isInEventsList) {
+//
+//     var newEvent = {
+//         event,
+//         date,
+//     };
+//
+//     if (!isInEventsList) {
+//         events.push(newEvent)
+//     }
+//
+//     var currentTime = dateValidator()
+//
+//     if (date === currentTime) {
+//         console.log(`It is ${event} time!`)
+//         return
+//     }
+//
+//     setTimeout(() => {
+//         setEvent(date, event, true)
+//     }, 1000)
+//
+// }
+//
+// setEvent('08.12.2020 20:06','new year')
 
-function dateValidator() {
+// var now = '9/12/2020'.split("/")
+// now[1] = now.splice(0,1, now[1])[0]
+// console.log(now.join('/'))
 
-    var currentDay = new Date().getDate()
-    var currentMonth = new Date().getMonth() + 1
-    var currentYear = new Date().getFullYear()
-    var currentHour = new Date().getHours()
-    var currentMin = new Date().getMinutes()
-
-    currentDay > 10 ? currentDay : currentDay = '0' + currentDay
-    currentMonth > 10 ? currentMonth : currentMonth = '0' + currentMonth
-    currentHour > 10 ? currentHour : currentHour = '0' + currentHour
-    currentMin > 10 ? currentMin : currentMin = '0' + currentMin
-
-    return `${currentDay}.${currentMonth}.${currentYear} ${currentHour}:${currentMin}`
-
+function datePreparation(userDate){
+    var time = userDate.split(" ")[1]
+    var date = userDate.split(" ")[0].split('.')
+    date[1] = date.splice(0,1, date[1])[0]
+    return  `${date.join('.')} ${time}`
 }
 
-var events=[]
+console.log(datePreparation('09.12.2020 20:09:41'))
 
-function setEvent(date, event, isInEventsList) {
-
-    var newEvent = {
-        event,
-        date,
-    };
-
-    if (!isInEventsList) {
-        events.push(newEvent)
-    }
-
-    var currentTime = dateValidator()
-
-    if (date === currentTime) {
-        console.log(`It is ${event} time!`)
-        return
-    }
-
-    setTimeout(() => {
-        setEvent(date, event, true)
-    }, 1000)
-
-}
-
-setEvent('08.12.2020 20:06','new year')
